fd66f88c1b96317bb82e1bc805eadc214556e9a4
Roberto <roberto.guarneros11@gmail.com>
2024-01-18 13:53:59 +0100

    First Commit
    First Level Created

---
+++ Assets/Scripts/DriveKart.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveKart : MonoBehaviour
+{
+    public float velocity;
+    public float jumpingStrength;
+    private Rigidbody2D rigidBody;
+    private CapsuleCollider2D boxCollider;
+
+    private float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.DrawRay(transform.position, Vector3.down * 0.9f, Color.red);
+
+        if (IsGrounded())
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        ProcessMovement();
+        ProcessJump();
+    }
+
+    void ProcessJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (coyoteTimeCounter > 0f)
+            {
+                rigidBody.AddForce(Vector2.up * jumpingStrength, ForceMode2D.Impulse);
+                coyoteTimeCounter = 0f;
+            }
+        }
+    }
+
+    void ProcessMovement()
+    {
+        float inputMovement = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(inputMovement * velocity, rigidBody.velocity.y);
+    }
+
+    bool IsGrounded()
+    {
+        float groundCheckRadius = 0.2f;
+        LayerMask groundLayer = LayerMask.GetMask("Ground"); // Set the appropriate layer for your ground
+
+        Collider2D hitCollider = Physics2D.OverlapCircle(transform.position, groundCheckRadius, groundLayer);
+
+        return hitCollider != null;
+    }
+
+}
